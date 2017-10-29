7798479b73d3dc8140d189c0445a91e9de42ce2c
Matt Zeunert <matt@mostlystatic.com>
2017-10-29 13:26:01 +0800

    Add delete todo item functionality

--- generate.js
+++ generate.js
@@ -46,6 +46,9 @@ body  {
 .created-checkbox-label:not(#created-checkbox-label-1) {
     display: none;
 }
+.deleted-checkbox {
+    display: none;
+}
 .todo {
     display: flex;
     width: 100%;
@@ -108,6 +111,9 @@ function generateTodo(i) {
         #mark-undone-checkbox-label-${i} {
             display: none;
         }
+        #deleted-checkbox-label-${i} {
+            display: none;
+        }
         #created-checkbox-${i}:checked ~ #done-checkbox-${i}:not(:checked) ~ #mark-done-checkbox-label-${i} {
             display: inline-block;
             order: ${getOrder(1)};
@@ -120,15 +126,28 @@ function generateTodo(i) {
             order: ${getOrder(90)};
         }
 
+        #created-checkbox-${i}:checked ~ #deleted-checkbox-label-${i} {
+            display: inline-block;
+        }
+
+        #deleted-checkbox-${i}:checked ~ .mark-done-checkbox-label,
+        #deleted-checkbox-${i}:checked ~ .mark-undone-checkbox-label,
+        #deleted-checkbox-${i}:checked ~ .deleted-checkbox-label,
+        #deleted-checkbox-${i}:checked ~ #todo-input-${i} {
+            display: none !important;
+        }
+
         .active-filter:target #created-checkbox-${i}:checked ~ #done-checkbox-${i}:checked ~ #todo-input-${i},
         .active-filter:target #created-checkbox-${i}:checked ~ #done-checkbox-${i}:checked ~ #mark-done-checkbox-label-${i},
-        .active-filter:target #created-checkbox-${i}:checked ~ #done-checkbox-${i}:checked ~ #mark-undone-checkbox-label-${i} {
+        .active-filter:target #created-checkbox-${i}:checked ~ #done-checkbox-${i}:checked ~ #mark-undone-checkbox-label-${i},
+        .active-filter:target #created-checkbox-${i}:checked ~ #done-checkbox-${i}:checked ~ .deleted-checkbox-label {
             display: none !important;
         }
 
         .completed-filter:target #created-checkbox-${i}:checked ~ #done-checkbox-${i}:not(:checked) ~ #todo-input-${i},
         .completed-filter:target #created-checkbox-${i}:checked ~ #done-checkbox-${i}:not(:checked) ~ #mark-done-checkbox-label-${i},
-        .completed-filter:target #created-checkbox-${i}:checked ~ #done-checkbox-${i}:not(:checked) ~ #mark-undone-checkbox-label-${i} {
+        .completed-filter:target #created-checkbox-${i}:checked ~ #done-checkbox-${i}:not(:checked) ~ #mark-undone-checkbox-label-${i},
+        .completed-filter:target #created-checkbox-${i}:checked ~ #done-checkbox-${i}:not(:checked) ~ .deleted-checkbox-label {
             display: none !important;
         }
     `
@@ -147,7 +166,9 @@ function generateTodo(i) {
         <div id="todo-${i}" class="todo">
             <input type="checkbox" id="created-checkbox-${i}" ${i <= 2 ? ' checked': ''} />
             <label for="created-checkbox-${i}" class="created-checkbox-label" id="created-checkbox-label-${i}">Add</label>
+            <input type="checkbox" class="deleted-checkbox" id="deleted-checkbox-${i}" />
             <input type="checkbox" class="done-checkbox" id="done-checkbox-${i}" ${i <= 1 ? ' checked': ''} />
+            <label for="deleted-checkbox-${i}" id="deleted-checkbox-label-${i}" class="deleted-checkbox-label">delete</label>
             <label for="done-checkbox-${i}" id="mark-done-checkbox-label-${i}" class="mark-done-checkbox-label"></label>
             <label for="done-checkbox-${i}" id="mark-undone-checkbox-label-${i}" class="mark-undone-checkbox-label"></label>
             <input type="text" value="${ {1: 'Build Todo CSS', 2: 'Eat cookies'}[i] || ''}" id="todo-input-${i}" class="todo-input" placeholder="What needs to be done?"/>
