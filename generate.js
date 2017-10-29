a4356e305f9c6ec328e0bfd8def95b7db6dbcf4d
Matt Zeunert <matt@mostlystatic.com>
2017-10-29 15:19:21 +0800

    Move CSS to separate file

--- generate.js
+++ generate.js
@@ -1,185 +1,6 @@
 const fs = require('fs')
 
 html = ''
-css = `
-#todo-1 {
-    position: relative;
-    padding-top: 65px;
-}
-body  {
-    counter-reset: items-left;
-}
-.todo-input {
-    white-space: pre-line;
-    word-break: break-all;
-    padding: 15px 60px 15px 15px;
-    margin-left: 45px;
-    display: block;
-    line-height: 1.2;
-    transition: color 0.4s;
-
-    border: none;
-    background: rgba(0, 0, 0, 0.003);
-    box-shadow: inset 0 -2px 1px rgba(0,0,0,0.03);
-    position: relative;
-    margin: 0;
-    width: calc(100% - 40px - 120px);
-    font-size: 24px;
-    font-family: inherit;
-    font-weight: inherit;
-    line-height: 1.4em;
-    border: 0;
-    outline: none;
-    color: inherit;
-    -webkit-font-smoothing: antialiased;
-    -moz-font-smoothing: antialiased;
-    font-smoothing: antialiased;
-}
-.created-checkbox-label {
-    position: absolute;
-    top: 0;
-    right: 0px;
-    width: 40px;
-    cursor: pointer;
-    background: #d6d6d6;
-    font-weight: bold;
-    padding: 22px;
-    padding-left: 10px;
-    padding-right: 10px;
-    text-align: center;
-}
-.created-checkbox-label:hover {
-    background: #ddd;
-}
-.todo-input:not(#todo-input-1) {
-    display: none;
-}
-.created-checkbox-label:not(#created-checkbox-label-1) {
-    display: none;
-}
-.deleted-checkbox {
-    display: none;
-}
-.todo {
-    display: flex;
-    width: 100%;
-    flex-wrap: wrap;
-
-}
-
-.active-filter:target .filter-active,
-.completed-filter:target .filter-completed,
-.completed-filter:not(:target) .active-filter:not(:target) .filter-all {
-    border-color: rgba(175, 47, 47, 0.2);
-}
-#items-left:before {
-    content: counter(items-left);
-}
-.mark-done-checkbox-label {
-    counter-increment: items-left;
-}
-
-.mark-done-checkbox-label,
-.mark-undone-checkbox-label {
-    cursor: pointer;
-}
-
-.deleted-checkbox-label {
-    display: none;
-	width: 40px;
-	height: 40px;
-	margin: auto 0;
-	font-size: 30px;
-	color: #cc9a9a;
-	transition: color 0.2s ease-out;
-    text-align: center;
-    opacity: 0;
-    padding-top: 12px;
-    padding-bottom: 11px;
-}
-.mark-done-checkbox-label,
-.mark-undone-checkbox-label {
-    padding-top: 10px;
-    padding-bottom: 9px;
-}
-
-.deleted-checkbox-label:hover {
-	color: #af5b5e;
-}
-
-.deleted-checkbox-label:after {
-	content: '×';
-}
-
-
-.deleted-checkbox-label {
-    cursor: pointer;
-}
-
-/* Styling for new todo item input */
-.created-checkbox:not(:checked) ~ .todo-input {
-    position: absolute;
-    top: 0;
-    padding-left: calc(15px + 40px);
-    box-shadow: inset 0 -2px 1px rgba(0,0,0,0.03);
-}
-.created-checkbox:not(:checked) ~ .todo-input,
-.created-checkbox:not(:checked) ~ .created-checkbox-label {
-    border-bottom: 1px solid #e6e6e6;
-}
-
-
-/* Hide mark as done/undone by default */
-.done-checkbox,
-.mark-done-checkbox-label,
-.mark-undone-checkbox-label {
-    display: none;
-}
-/* Show mark as done if item is created and not marked as done yet */
-.created-checkbox:checked ~ .done-checkbox:not(:checked) ~ .mark-done-checkbox-label {
-    display: inline-block;
-    order: 1
-}
-/* Show mark as undone if item is created and marked as done */
-.created-checkbox:checked ~ .done-checkbox:checked ~ .mark-undone-checkbox-label {
-    display: inline-block;
-    order: 1
-}
-
-/* Show the delete icon on hover over any of the item elements */
-.created-checkbox:checked ~ .deleted-checkbox-label:hover,
-.created-checkbox:checked ~ .mark-undone-checkbox-label:hover ~ .deleted-checkbox-label,
-.created-checkbox:checked ~ .mark-done-checkbox-label:hover ~ .deleted-checkbox-label,
-.created-checkbox:checked ~ .todo-input:hover ~ .deleted-checkbox-label {
-    opacity: 1;
-}
-
-/* If item is deleted hide its elements */
-.deleted-checkbox:checked ~ .mark-done-checkbox-label,
-.deleted-checkbox:checked ~ .mark-undone-checkbox-label,
-.deleted-checkbox:checked ~ .deleted-checkbox-label,
-.deleted-checkbox:checked ~ .todo-input {
-    display: none !important;
-}
-
-/* Hide items marked as done if filter is active */
-.active-filter:target .created-checkbox:checked ~ .done-checkbox:checked ~ .todo-input,
-.active-filter:target .created-checkbox:checked ~ .done-checkbox:checked ~ .mark-done-checkbox-label,
-.active-filter:target .created-checkbox:checked ~ .done-checkbox:checked ~ .mark-undone-checkbox-label,
-.active-filter:target .created-checkbox:checked ~ .done-checkbox:checked ~ .deleted-checkbox-label {
-    display: none !important;
-}
-
-/* Hide items not marked done if filter is completed */
-.completed-filter:target .created-checkbox:checked ~ .done-checkbox:not(:checked) ~ .todo-input,
-.completed-filter:target .created-checkbox:checked ~ .done-checkbox:not(:checked) ~ .mark-done-checkbox-label,
-.completed-filter:target .created-checkbox:checked ~ .done-checkbox:not(:checked) ~ .mark-undone-checkbox-label,
-.completed-filter:target .created-checkbox:checked ~ .done-checkbox:not(:checked) ~ .deleted-checkbox-label {
-    display: none !important;
-}
-
-
-`
 
 const MAX_ITEMS = 50
 
@@ -188,70 +9,19 @@ function generateTodo(i) {
         return ''
     }
 
-    function getOrder(orderWithinItem) {
-        if (orderWithinItem > 100) {
-            throw Error('no')
-        }
-        return 1000 + i * 100 + orderWithinItem
-    }
-
-    css += `
-        #todo-input-${i} {
-            order: ${getOrder(10)};
-        }
-        #created-checkbox-${i} {
-            display: none;
-        }
-
-        #created-checkbox-${i}:checked + input {
-            border: none;
-        }
-        #created-checkbox-${i}:checked + input {
-            order: ${getOrder(2)};
-        }
-        #created-checkbox-${i}:checked ~ #created-checkbox-label-${i} {
-            display: none !important;
-        }
-
-        #deleted-checkbox-label-${i} {
-            display: none;
-        }
-
-        #todo-${i + 1} {
-            order: ${getOrder(90)};
-        }
-
-        #created-checkbox-${i}:checked ~ #deleted-checkbox-label-${i} {
-            display: inline-block;
-            order: ${getOrder(11)}
-        }
-    `
-    if (i > 1) {
-        const previousI = i - 1;
-        css += `
-            #created-checkbox-${previousI}:checked ~ #todo-${i} > #todo-input-${i},
-            #created-checkbox-${previousI}:checked ~ #todo-${i} > #created-checkbox-label-${i} {
-                display: inline-block;
-            }
-
-        `
-    }
-
     return `
         <div id="todo-${i}" class="todo">
             <input type="checkbox" class="created-checkbox" id="created-checkbox-${i}" ${i <= 2 ? ' checked': ''} />
-            <label for="created-checkbox-${i}" class="created-checkbox-label" id="created-checkbox-label-${i}">Add</label>
+            <label for="created-checkbox-${i}" class="created-checkbox-label">Add</label>
             <input type="checkbox" class="deleted-checkbox" id="deleted-checkbox-${i}" />
             <input type="checkbox" class="done-checkbox" id="done-checkbox-${i}" ${i <= 1 ? ' checked': ''} />
-            <label for="done-checkbox-${i}" id="mark-done-checkbox-label-${i}" class="mark-done-checkbox-label"></label>
-            <label for="done-checkbox-${i}" id="mark-undone-checkbox-label-${i}" class="mark-undone-checkbox-label"></label>
-            <input type="text" value="${ {1: 'Build Todo CSS', 2: 'Eat cookies'}[i] || ''}" id="todo-input-${i}" class="todo-input" placeholder="What needs to be done?"/>
-            <label for="deleted-checkbox-${i}" id="deleted-checkbox-label-${i}" class="deleted-checkbox-label"></label>
+            <label for="done-checkbox-${i}" class="mark-done-checkbox-label"></label>
+            <label for="done-checkbox-${i}" class="mark-undone-checkbox-label"></label>
+            <input type="text" value="${ {1: 'Build Todo CSS', 2: 'Eat cookies'}[i] || ''}" class="todo-input" placeholder="What needs to be done?"/>
+            <label for="deleted-checkbox-${i}" class="deleted-checkbox-label"></label>
             ${generateTodo(i + 1)}
         </div>
     `
-
-
 }
 
 html += `<!doctype html>
@@ -260,6 +30,7 @@ html += `<!doctype html>
         <meta charset="utf-8">
 		<title>Todo CSS</title>
         <link rel="stylesheet" href="./todomvc-app.css" />
+        <link rel="stylesheet" href="./todo.css" />
     </head>
     <body>
         <div id="/completed" class="completed-filter">
@@ -268,7 +39,7 @@ html += `<!doctype html>
                     <header class="header">
                         <h1>todos</h1>
                     </header>
-                    ${generateTodo(1)}
+                    ${generateTodo(0)}
                     <footer class="footer" style="display: block;">
             			<span class="todo-count"><strong id="items-left"></strong> items left</span>
             			<ul class="filters">
@@ -286,8 +57,8 @@ html += `<!doctype html>
                 </section>
                 <div style="line-height: 18px;background: #fafafa;border: 1px solid #eaeaea;padding: 15px;">
                     <p style="margin-top: 0">
-                        This todo app is built using just HTML and CSS, no JavaScript. It works by using pre-rendered HTML,
-                        the ~ combinator, and the :checked and :target pseudo selectors.
+                        This is a todo app built using just HTML and CSS, no JavaScript. It works by using pre-rendered HTML,
+                        the ~ combinator, CSS counters, and the :checked and :target pseudo selectors.
                     </p>
                     <p style="margin-bottom: 0">
                         Styles based on <a href="http://todomvc.com/">TodoMVC</a>.
@@ -297,6 +68,5 @@ html += `<!doctype html>
         </div>
     </body>
 </html>`
-html += `<style>${css}</style>`
 
 fs.writeFileSync('todo.html', html);
